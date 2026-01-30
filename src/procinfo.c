94e9acd67d8780140e2e030b2ca853624077a6c6
kels <cavin.a.kelsey@gmail.com>
2026-01-30 16:50:45 -0600

    Some refactoring, code cleanup in procinfo

--- src/procinfo.c
+++ src/procinfo.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <sys/utsname.h>
 #include <string.h>
-
+#include "procinfo.h"
 
 /*--global constants--*/
 char LINE[256];
@@ -28,34 +28,48 @@ static int isnum(const char*s){
 static long get_proc_long(const char *pid, const char *pattern){
 	//todo break up into functions
 	char proc_file[100];
+    build_proc_path(proc_file, pid);
 
-	strcpy(proc_file, "/proc/");
-	strcat(proc_file,pid);
-	strcat(proc_file,"/status");
-	
-	printf("LOG - opening proc file with path: %s \n",proc_file);
+    printf("LOG - opening proc file with path: %s \n",proc_file);
 
 	FILE* file = fopen(proc_file,"r");
 
-	if(!file){
-		perror("Unable to open file.\n");
-		return -1;
-	}
+	exit_if_file_not_exists(file);
 
-	long val = -1;
+	long pattern_matched_value = -1;
 
-	while(fgets(LINE,LINE_SIZE,file)){
-		if(sscanf(LINE,pattern, &val)==1){
+	while(not_file_end(file)){
+		if(sscanf(LINE, pattern, &pattern_matched_value)==1){
 			break;
 		}
 	}
 
 	fclose(file);
-	return val;
+	return pattern_matched_value;
 }
 
-int main(int c,char**v){
-	if(c!=2 || !isnum(v[1])){ 
+char * not_file_end(FILE * file)
+{
+return fgets(LINE,LINE_SIZE,file);
+}
+
+static void exit_if_file_not_exists(FILE *file){
+	if(!file){
+		perror("Unable to open file.\n");
+		exit(1);
+	}
+}
+
+void build_proc_path(char proc_file[100], const char *pid)
+{
+    strcpy(proc_file, "/proc/");
+    strcat(proc_file, pid);
+    strcat(proc_file, "/status");
+}
+
+int main(int c, char **v)
+{
+    if(c!=2 || !isnum(v[1])){ 
 		usage(v[0]);
 	}
 	//todo extend to all required printouts
---
+++ src/procinfo.h
@@ -0,0 +1,9 @@
+#pragma once
+static void build_proc_path(char proc_file[100], const char *pid);
+static void usage(const char *a);
+static int isnum(const char*s);
+static long get_proc_long(const char *pid, const char *pattern);
+
+static char * not_file_end(FILE * file);
+
+static void exit_if_file_not_exists(FILE *file);
